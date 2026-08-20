ea1be569ae341bde1fd73cc2beaee010717bc4cb
Jelli <contactjellifeesh@gmail.com>
2026-08-20 19:16:04 +0930

    Update main.tsx to use GlobalStyle
    Replaced ThemeProvider with GlobalStyle for styling.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import { win95 } from '@react95/core';
+import { GlobalStyle } from '@react95/core';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={win95}>
-      <App />
-    </ThemeProvider>
+    <GlobalStyle />
+    <App />
   </React.StrictMode>
 );
