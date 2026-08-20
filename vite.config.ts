4164f262ebb8ace86b83f44a1fb51baaa7e788c3
Jelli <contactjellifeesh@gmail.com>
2026-08-20 13:40:17 +0930

    Configure build output directory to 'docs'
    Added build configuration to output production build to the 'docs' directory.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/AngelikaDael/', 
+  build: {
+    outDir: 'docs', // Outputs production build to /docs
+  },
 })
