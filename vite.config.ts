df9cc1cd1fb81786ccd8d3778ec5c80bbd33d5b4
Jelli <contactjellifeesh@gmail.com>
2026-08-20 13:49:22 +0930

    Simplify vite.config.ts by removing build settings
    Removed build configuration and output directory.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AngelikaDael/', 
-  build: {
-    outDir: 'docs', // Outputs production build to /docs
-  },
+  base: '/AngelikaDael/',
 })
