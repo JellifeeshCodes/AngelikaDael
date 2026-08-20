4050767692a76c5dfde86196ac57d0766d3ccad9
Jelli <contactjellifeesh@gmail.com>
2026-08-20 15:39:54 +0930

    Import GlobalStyle in main.tsx
    Added GlobalStyle import for consistent styling.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@react95/core/GlobalStyle';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
