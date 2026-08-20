4e0f681c631a47445e64dad97d099b0e919e2c01
Jelli <contactjellifeesh@gmail.com>
2026-08-20 19:10:22 +0930

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { win95 } from '@react95/core';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={win95}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
