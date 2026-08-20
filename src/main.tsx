1599948f005b4c9024b02958ee6cab8e6c1536ee
Jelli <contactjellifeesh@gmail.com>
2026-08-20 15:36:50 +0930

    Remove ThemeProvider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider, GlobalStyle } from '@react95/core';
+import { GlobalStyle } from '@react95/core';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <GlobalStyle />
+    <App />
   </React.StrictMode>
 );
