83679e65a3af920c527ec2a2475517352752148b
Jelli <contactjellifeesh@gmail.com>
2026-08-20 14:26:50 +0930

    Refactor App component and improve structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AGENTS, ClippyProvider } from "@react95/clippy";
-import { Amovie2, Inetcpl1313, Joy102, Wordpad, Mail, CdMusic } from "@react95/icons";
+import { Amovie2, Inetcpl1313, Joy102, Wordpad, Mail } from "@react95/icons";
 
 import Login from "./components/Login";
 import WindowBar from "./components/WindowBar";
@@ -11,16 +11,23 @@ import Game from "./components/Game";
 import ArtGallery from "./components/ArtGallery";
 import { useAuth } from "./store/auth";
 
-// VITE FIX: Use a relative path to your assets folder
+// VITE FIX: Relative path to assets folder for central logo
 import winLogo from './assets/logo.png'; 
 
 function App() {
-  // Fixed typo from 'authinicated' to 'authenticated'
   const authenticated = useAuth((state) => state.authenticated);
 
   return (
-    <div style={{ width: "100vw", height: "100vh", backgroundColor: "#098684", position: "relative", overflow: "hidden" }}>
-      {/* Centered Background Logo */}
+    <div 
+      style={{ 
+        width: "100vw", 
+        height: "100vh", 
+        backgroundColor: "#098684", 
+        position: "relative", 
+        overflow: "hidden" 
+      }}
+    >
+      {/* Centered Windows 95 Background Logo */}
       <img 
         src={winLogo} 
         alt="Windows 95 Logo" 
@@ -31,7 +38,7 @@ function App() {
           left: "50%",
           transform: "translate(-50%, -70%)",
           zIndex: 0,
-          pointerEvents: "none" // Icons can still be clicked over the logo
+          pointerEvents: "none"
         }}
       />
 
@@ -39,19 +46,60 @@ function App() {
         <Login />
       ) : (
         <ClippyProvider agentName={AGENTS.MERLIN}>
-          <div style={{ padding: "20px", display: "flex", flexDirection: "column", gap: "20px", zIndex: 1, position: "relative" }}>
+          <div 
+            style={{ 
+              padding: "20px", 
+              display: "flex", 
+              flexDirection: "column", 
+              gap: "20px", 
+              zIndex: 1, 
+              position: "relative" 
+            }}
+          >
             <DesktopIcon icon={<Amovie2 variant="32x32_4" />} name="Video">
-              <iframe width="420" height="315" src="https://www.youtube.com/embed/v4yk_eXNbjo" allowFullScreen title="Video Player" />
+              <iframe 
+                width="420" 
+                height="315" 
+                src="https://www.youtube.com/embed/v4yk_eXNbjo" 
+                allowFullScreen 
+                title="Video Player" 
+              />
             </DesktopIcon>
 
-            <DesktopIcon icon={<Inetcpl1313 variant="32x32_4"/>} name="Browser">
-              <iframe width={800} height={500} src="https://swisscows.com" title="Browser" />
+            <DesktopIcon icon={<Inetcpl1313 variant="32x32_4" />} name="Browser">
+              <iframe 
+                width={800} 
+                height={500} 
+                src="https://swisscows.com" 
+                title="Browser" 
+              />
             </DesktopIcon>
 
-            <DesktopIcon width={650} icon={<Wordpad variant="32x32_4"/>} name="Resume"><Resume/></DesktopIcon>
-            <DesktopIcon width={650} icon={<img src="/art.png" alt="Art Gallery" style={{ width: '32px', height: '32px' }} />} name="Art Gallery"><ArtGallery/></DesktopIcon>
-            <DesktopIcon width={400} height={400} icon={<Joy102 variant="32x32_4"/>} name="Game"><Game/></DesktopIcon>
-            <DesktopIcon width={400} icon={<Mail variant="32x32_4"/>} name="Contact"><Contact/></DesktopIcon>
+            <DesktopIcon width={650} icon={<Wordpad variant="32x32_4" />} name="Resume">
+              <Resume />
+            </DesktopIcon>
+
+            <DesktopIcon 
+              width={650} 
+              icon={
+                <img 
+                  src="/art.png" 
+                  alt="Art Gallery" 
+                  style={{ width: '32px', height: '32px', display: 'block' }} 
+                />
+              } 
+              name="Art Gallery"
+            >
+              <ArtGallery />
+            </DesktopIcon>
+
+            <DesktopIcon width={400} height={400} icon={<Joy102 variant="32x32_4" />} name="Game">
+              <Game />
+            </DesktopIcon>
+
+            <DesktopIcon width={400} icon={<Mail variant="32x32_4" />} name="Contact">
+              <Contact />
+            </DesktopIcon>
           </div>
           <WindowBar />
         </ClippyProvider>
