80c935032d2252e928ee194f3fa12759cde5812e
Jelli <contactjellifeesh@gmail.com>
2026-08-20 18:52:36 +0930

    Reformat main.tsx for consistency and readability

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '@react95/core/dist/styles.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
